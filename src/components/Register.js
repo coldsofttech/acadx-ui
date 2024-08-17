aa12d04248f3fb4f53eedc029606e5edbf33927c
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2024-08-17 20:23:19 +0100

    Added Color Palette

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import config from '../config';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.slim.min.js';
@@ -9,15 +7,11 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import '../App.css';
 
 function Register() {
-    const [titles, setTitles] = useState([]);
     const [formData, setFormData] = useState({
-        title: '',
         firstName: '',
-        middleName: '',
         lastName: '',
         dob: '',
         email: '',
-        confirmEmail: '',
         password: '',
         confirmPassword: '',
         termsAccepted: false
@@ -36,43 +30,17 @@ function Register() {
         // Submission logic here
     };
 
-    useEffect(() => {
-        const fetchTitles = async () => {
-            try {
-                const response = await axios.get(`${config.ACADX_API_URL}/titles`);
-                setTitles(response.data);
-            } catch (error) {
-                console.error('Error fetching titles:', error);
-            }
-        };
-
-        fetchTitles();
-    }, []);
-
     return (
         <div className='row justify-content-center'>
             <div className='register-container col-md-10 col-lg-8'>
                 <div className='register-box'>
-                    <h3 className='text-center mb-4'>Register</h3>
+                    <h3 className='text-center mb-4 h3-header'>Register</h3>
                     <form onSubmit={handleSubmit}>
                         <div className='row'>
                             <div className='col-md-6'>
                                 <div className='form-group mb-3' style={{ textAlign: 'left' }}>
-                                    <label htmlFor='title' className='form-label'>Title</label>
-                                    <select id='title' name='title' className='form-control' value={formData.title} onChange={handleInputChange} required>
-                                        <option value=''>Select your title</option>
-                                        {titles.map(title => (
-                                            <option key={title} value={title}>{title}</option>
-                                        ))}
-                                    </select>
-                                </div>
-                                <div className='form-group mb-3' style={{ textAlign: 'left' }}>
                                     <label htmlFor='firstName' className='form-label'>First Name</label>
                                     <input type='text' className='form-control' id='firstName' name='firstName' value={formData.firstName} onChange={handleInputChange} placeholder='Enter first name' required />
-                                </div>
-                                <div className='form-group mb-3' style={{ textAlign: 'left' }}>
-                                    <label htmlFor='middleName' className='form-label'>Middle Name</label>
-                                    <input type='text' className='form-control' id='middleName' name='middleName' value={formData.middleName} onChange={handleInputChange} placeholder='Enter middle name' />
                                 </div>
                                 <div className='form-group mb-3' style={{ textAlign: 'left' }}>
                                     <label htmlFor='lastName' className='form-label'>Last Name</label>
@@ -89,10 +57,6 @@ function Register() {
                                     <input type='email' className='form-control' id='email' name='email' value={formData.email} onChange={handleInputChange} placeholder='Enter email address' required />
                                 </div>
                                 <div className='form-group mb-3' style={{ textAlign: 'left' }}>
-                                    <label htmlFor='confirmEmail' className='form-label'>Confirm email address</label>
-                                    <input type='email' className='form-control' id='confirmEmail' name='confirmEmail' value={formData.confirmEmail} onChange={handleInputChange} placeholder='Enter email address' required />
-                                </div>
-                                <div className='form-group mb-3' style={{ textAlign: 'left' }}>
                                     <label htmlFor='password' className='form-label'>Password</label>
                                     <input type='password' className='form-control' id='password' name='password' value={formData.password} onChange={handleInputChange} placeholder='Enter password' required />
                                 </div>
@@ -100,19 +64,25 @@ function Register() {
                                     <label htmlFor='confirmPassword' className='form-label'>Confirm password</label>
                                     <input type='password' className='form-control' id='confirmPassword' name='confirmPassword' value={formData.confirmPassword} onChange={handleInputChange} placeholder='Enter password' required />
                                 </div>
-                                <div className='form-group mb-3' style={{ textAlign: 'right' }}>
-                                    <input type='checkbox' id='termsAccepted' name='termsAccepted' checked={formData.termsAccepted} onChange={handleInputChange} required />
-                                    <label htmlFor='termsAccepted' className='form-check-label ms-2 text-secondary'>
-                                        I accept the <a href='/terms-and-conditions' className='text-secondary' target='_blank' rel='noopener noreferrer'>Terms and Conditions.</a>
-                                    </label>
-                                </div>
-                                <div className='form-group mb-3'>
-                                    <button type='submit' className='btn btn-primary btn-block' style={{ width: '100%' }}>Register</button>
-                                </div>
-                                <div className='form-group mt-3' style={{ textAlign: 'right' }}>
-                                    <Link to='/register-sso' className='text-secondary'>Register with SSO?</Link>
-                                </div>
                             </div>
+                        </div>
+                        <div className='row'>
+                            <div className='form-group mb-3' style={{ textAlign: 'left' }}>
+                                <input type='checkbox' id='termsAccepted' name='termsAccepted' checked={formData.termsAccepted} onChange={handleInputChange} required />
+                                <label htmlFor='termsAccepted' className='form-check-label ms-2 text-link'>
+                                    I consent to the collection and use of my data described in the <a href='/terms-and-conditions' className='text-link' target='_blank' rel='noopener noreferrer'>Terms and Conditions</a>.
+                                </label>
+                            </div>
+                            <div className='form-group mb-3'>
+                                <button type='submit' id='submit' className='btn btn-primary btn-block' style={{ width: '50%' }}>Register</button>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className='form-group mt-3' style={{ textAlign: 'right' }}>
+                            <Link to='/register-sso' className='text-link'>Register with SSO?</Link>
+                        </div>
+                        <div className='form-group mt-3' style={{ textAlign: 'right' }}>
+                            <Link to='/login' className='text-link'>Already have an account?</Link>
                         </div>
                     </form>
                 </div>
