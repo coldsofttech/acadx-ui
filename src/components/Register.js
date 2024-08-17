b63ca2579ffe7abbb7da4078980e6d85a36924a8
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2024-08-17 11:06:08 +0100

    Initial Commit

---
+++ src/components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Register() {
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <p>Registration form goes here...</p>
+    </div>
+  );
+}
+
+export default Register;
